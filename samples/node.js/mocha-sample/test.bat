a08e4f5592d05aa7baeb6d4527e6e83f2b49cc7b
phi <phi1618jp@gmail.com>
2012-05-03 15:00:56 +0900

    mocha サンプル追加

---
+++ samples/node.js/mocha-sample/test.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem call mocha --reporter spec test.js
+call mocha --reporter spec test.js
+rem call mocha test.js
+
+pause
---
+++ samples/node.js/mocha-sample/test.js
@@ -0,0 +1,31 @@
+function assert(expr, msg) {
+    if (!expr) throw new Error(msg || 'failed');
+}
+
+describe('Number', function() {
+    describe('calc', function() {
+        it('add', function() {
+            assert(4+8 == 12);
+        });
+        it('sub', function() {
+            assert(4-8 == -4);
+        });
+        it('mul', function() {
+            assert(16*2 == 32);
+        });
+        it('div', function() {
+            assert(8/2 == 4);
+        });
+    });
+});
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            var arr = [1, 2, 3];
+            assert(arr.indexOf(3) == 2);
+            assert(arr.indexOf(5) ==-1);
+            assert(arr.indexOf(0) ==-1);
+        });
+    });
+});
